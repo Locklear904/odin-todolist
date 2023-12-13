bc9edc3be809f54c6e5218fe8f1f794fd544e0cd
Rich Barnes <locklear904@gmail.com>
2023-12-13 10:34:33 -0500

    Refactor Various Functions
    Various functions are now more separated logically to help make
    them as modular and focused as possible.

--- src/scripts/appLogic.js
+++ src/scripts/appLogic.js
@@ -1,4 +1,4 @@
-import { displaySidebarContent, clearNewProjectForm, clearTodos, clearNewTodoForm, displayTodos, clearMain, clearSidebar, displayProject } from "./display";
+import { displaySidebarContent, clearNewProjectForm, clearTodos, clearNewTodoForm, displayTodos, clearMain, clearSidebar, displayProject, createTodoEditForm, showTodoDetails, createEditProjectForm } from "./display";
 
 class Project {
     constructor(title = "Default", description = "") {
@@ -45,55 +45,36 @@ function createNewTodo() {
     displaySidebarContent(projects);
 }
 
-function editProject() {
-    if (this.textContent === "Edit") {
-        const projectHeader = document.querySelector('.projectHeader');
-        const projectDiv = document.querySelector('.projectDiv');
-        const projectIndex = projectDiv.getAttribute('data-index');
-        const currentProject = projects[projectIndex];
-        const projectTitleLabel = document.createElement('label');
-        projectTitleLabel.setAttribute('id', 'projectTitleLabel');
-        projectTitleLabel.setAttribute('for', 'projectTitleEdit');
-        projectTitleLabel.textContent = "Project Title: "
-        projectHeader.prepend(projectTitleLabel);
-        const projectTitle = document.querySelector('.projectTitle');
-        const projectTitleInput = document.createElement('input');
-        projectTitleInput.setAttribute('id', 'projectTitleEdit');
-        projectTitleInput.value = currentProject.title;
-        projectHeader.replaceChild(projectTitleInput, projectTitle);
-        const projectDescription = document.querySelector('.projectDescription');
-        const projectDescriptionLabel = document.createElement('label');
-        projectDescriptionLabel.setAttribute('id', 'projectDescriptionLabel');
-        projectDescriptionLabel.setAttribute('for', 'projectDescriptionEdit');
-        projectDescriptionLabel.textContent = "Project Description: ";
-        projectDiv.insertBefore(projectDescriptionLabel, projectDescription);
-        const projectDescriptionInput = document.createElement('textarea');
-        projectDescriptionInput.setAttribute('id', 'projectDescriptionEdit');
-        projectDescriptionInput.value = currentProject.description; 
-        projectDiv.replaceChild(projectDescriptionInput, projectDescription);
-        this.textContent = "Submit";
-    } else if (this.textContent = "Submit") {
-        const projectDiv = document.querySelector('.projectDiv');
-        const projectHeader = document.querySelector('.projectHeader');
-        const projectIndex = projectDiv.getAttribute('data-index');
-        const currentProject = projects[projectIndex];
-        const projectTitleEdit = document.querySelector('#projectTitleEdit');
-        const projectDescriptionEdit = document.querySelector('#projectDescriptionEdit');
-        currentProject.title = projectTitleEdit.value;
-        currentProject.description = projectDescriptionEdit.value;
-        const projectTitleLabel = document.querySelector('#projectTitleLabel');
-        projectTitleLabel.remove();
-        const projectDescriptionLabel = document.querySelector('#projectDescriptionLabel');
-        projectDescriptionLabel.remove();
-        const projectTitle = document.createElement('h2');
-        projectTitle.setAttribute('class', 'projectTitle');
-        projectTitle.textContent = currentProject.title;
-        projectHeader.replaceChild(projectTitle, projectTitleEdit);
-        const projectDescription = document.createElement('p');
-        projectDescription.setAttribute('class', 'projectDescription');
-        projectDescription.textContent = currentProject.description;
-        projectDiv.replaceChild(projectDescription, projectDescriptionEdit);
-        this.textContent = "Edit";
+function submitProjectEdit(projectHeader, projectDiv, currentProject) {
+    const projectTitleEdit = document.querySelector('#projectTitleEdit');
+    const projectDescriptionEdit = document.querySelector('#projectDescriptionEdit');
+    currentProject.title = projectTitleEdit.value;
+    currentProject.description = projectDescriptionEdit.value;
+    const projectTitleLabel = document.querySelector('#projectTitleLabel');
+    projectTitleLabel.remove();
+    const projectDescriptionLabel = document.querySelector('#projectDescriptionLabel');
+    projectDescriptionLabel.remove();
+    const projectTitle = document.createElement('h2');
+    projectTitle.setAttribute('class', 'projectTitle');
+    projectTitle.textContent = currentProject.title;
+    projectHeader.replaceChild(projectTitle, projectTitleEdit);
+    const projectDescription = document.createElement('p');
+    projectDescription.setAttribute('class', 'projectDescription');
+    projectDescription.textContent = currentProject.description;
+    projectDiv.replaceChild(projectDescription, projectDescriptionEdit);
+}
+
+function editProject(event) {
+    const projectHeader = document.querySelector('.projectHeader');
+    const projectDiv = document.querySelector('.projectDiv');
+    const projectIndex = projectDiv.getAttribute('data-index');
+    const currentProject = projects[projectIndex];
+    if (event.target.textContent === "Edit") {
+        createEditProjectForm(projectHeader, projectDiv, currentProject);
+        event.target.textContent = "Submit";
+    } else if (event.target.textContent = "Submit") {
+        submitProjectEdit(projectHeader, projectDiv, currentProject);
+        event.target.textContent = "Edit";
     }
 }
 
@@ -127,32 +108,7 @@ function deleteProject() {
 
 function toggleTodoDetails(event) {
     if (event.target.textContent === "Show Details") {
-        const projectDiv = document.querySelector('.projectDiv');
-        const projectIndex = projectDiv.getAttribute('data-index');
-        const currentProject = projects[projectIndex];
-        const todoDiv = event.target.closest('.todoDiv');
-        const todoIndex = todoDiv.getAttribute('data-index');
-        const todo = currentProject.todos[todoIndex];
-        const todoDetailsDiv = document.createElement('div');
-        todoDetailsDiv.setAttribute('class', 'todoDetailsDiv');
-        todoDiv.appendChild(todoDetailsDiv);
-        let todoInfoDiv = document.createElement('div');
-        todoInfoDiv.setAttribute('class', 'todoInfoDiv');
-        todoDetailsDiv.appendChild(todoInfoDiv);
-        let todoDueDate = document.createElement('time');
-        todoDueDate.setAttribute('class', 'todoDueDate');
-        todoDueDate.setAttribute('datetime', todo.dueDate);
-        todoDueDate.textContent = `Due Date: ${todo.dueDate}`;
-        todoInfoDiv.appendChild(todoDueDate);
-        let todoPriority = document.createElement('p');
-        todoPriority.setAttribute('class', 'todoPriority');
-        todoPriority.textContent = `Priority: ${todo.priority}`;
-        todoInfoDiv.appendChild(todoPriority);
-        let todoDescription = document.createElement('p');
-        todoDescription.setAttribute('class', 'todoDescription');
-        todoDescription.textContent = todo.description;
-        todoDetailsDiv.appendChild(todoDescription);
-        event.target.textContent = "Hide Details";
+        showTodoDetails(event, projects);
     } else {
         const todoDiv = event.target.closest('.todoDiv');
         todoDiv.removeChild(todoDiv.lastElementChild);
@@ -160,84 +116,31 @@ function toggleTodoDetails(event) {
     }
 }
 
-function editTodo(event) {
-    if (event.target.textContent === "Edit") {
-        const projectDiv = document.querySelector('.projectDiv');
-        const projectIndex = projectDiv.getAttribute('data-index');
-        const currentProject = projects[projectIndex];
-        const todoDiv = event.target.closest('.todoDiv');
-        const todoIndex = todoDiv.getAttribute('data-index');
-        const todo = currentProject.todos[todoIndex];
-        const todoEditDiv = document.createElement('div');
-        todoEditDiv.setAttribute('id', `todoEditDiv${todoIndex}`);
-        todoEditDiv.setAttribute('class', 'todoEditDiv');
-        todoDiv.appendChild(todoEditDiv);
-        const todoEditTitleLabel = document.createElement('label');
-        todoEditTitleLabel.setAttribute('for', `todoEditTitle${todoIndex}`);
-        todoEditTitleLabel.textContent = "Todo Title: "
-        todoEditDiv.appendChild(todoEditTitleLabel);
-        const todoEditTitle = document.createElement('input');
-        todoEditTitle.setAttribute('id', `todoEditTitle${todoIndex}`);
-        todoEditTitle.value = todo.title;
-        todoEditDiv.appendChild(todoEditTitle);
-        const todoEditDescriptionLabel = document.createElement('label');
-        todoEditDescriptionLabel.setAttribute('for', `todoEditDescription${todoIndex}`);
-        todoEditDescriptionLabel.textContent = "Todo Description: ";
-        todoEditDiv.appendChild(todoEditDescriptionLabel);
-        const todoEditDescription = document.createElement('textarea');
-        todoEditDescription.setAttribute('id', `todoEditDescription${todoIndex}`);
-        todoEditDescription.value = todo.description;
-        todoEditDiv.appendChild(todoEditDescription);
-        const todoEditPriorityLabel = document.createElement('label');
-        todoEditPriorityLabel.setAttribute('for', `todoEditPriority${todoIndex}`);
-        todoEditPriorityLabel.textContent = "Todo Priority: ";
-        todoEditDiv.appendChild(todoEditPriorityLabel);
-        const todoEditPriority = document.createElement('select');
-        todoEditPriority.setAttribute('id', `todoEditPriority${todoIndex}`);
-        todoEditDiv.appendChild(todoEditPriority);
-        const todoEditPriorityHigh = document.createElement('option');
-        todoEditPriorityHigh.setAttribute('value', 'High');
-        todoEditPriorityHigh.textContent = "High";
-        todoEditPriority.appendChild(todoEditPriorityHigh);
-        const todoEditPriorityMedium = document.createElement('option');
-        todoEditPriorityMedium.setAttribute('value', 'Medium');
-        todoEditPriorityMedium.textContent = "Medium";
-        todoEditPriority.appendChild(todoEditPriorityMedium);
-        const todoEditPriorityLow = document.createElement('option');
-        todoEditPriorityLow.setAttribute('value', 'Low');
-        todoEditPriorityLow.textContent = "Low";
-        todoEditPriority.appendChild(todoEditPriorityLow);
-        todoEditPriority.value = todo.priority;
-        const todoEditDateLabel = document.createElement('label');
-        todoEditDateLabel.setAttribute('for', `todoEditDate${todoIndex}`);
-        todoEditDateLabel.textContent = "Due Date: ";
-        todoEditDiv.appendChild(todoEditDateLabel);
-        const todoEditDate = document.createElement('input');
-        todoEditDate.setAttribute('id', `todoEditDate${todoIndex}`);
-        todoEditDate.setAttribute('type', 'date');
-        todoEditDate.value = todo.dueDate;
-        todoEditDiv.appendChild(todoEditDate);
+function submitTodoEdit(todoIndex, todo) {
+    const todoEditTitle = document.querySelector(`#todoEditTitle${todoIndex}`);
+    todo.title = todoEditTitle.value;
+    const todoEditDescription = document.querySelector(`#todoEditDescription${todoIndex}`);
+    todo.description = todoEditDescription.value;
+    const todoEditPriority = document.querySelector(`#todoEditPriority${todoIndex}`);
+    todo.priority = todoEditPriority.value;
+    const todoEditDate = document.querySelector(`#todoEditDate${todoIndex}`);
+    todo.dueDate = todoEditDate.value;
+}
 
+function editTodo(event) {
+    const projectDiv = document.querySelector('.projectDiv');
+    const projectIndex = projectDiv.getAttribute('data-index');
+    const currentProject = projects[projectIndex];
+    const todoDiv = event.target.closest('.todoDiv');
+    const todoIndex = todoDiv.getAttribute('data-index');
+    const todo = currentProject.todos[todoIndex];
+    if (event.target.textContent === "Edit") {
+        createTodoEditForm(todoDiv, todoIndex, todo);
         event.target.textContent = "Submit";
     } else if (event.target.textContent === "Submit") {
-        const projectDiv = document.querySelector('.projectDiv');
-        const projectIndex = projectDiv.getAttribute('data-index');
-        const currentProject = projects[projectIndex];
-        const todoDiv = event.target.closest('.todoDiv');
-        const todoIndex = todoDiv.getAttribute('data-index');
-        const todo = currentProject.todos[todoIndex];
-        const todoEditDiv = document.querySelector(`#todoEditDiv${todoIndex}`);
-        const todoEditTitle = document.querySelector(`#todoEditTitle${todoIndex}`);
-        todo.title = todoEditTitle.value;
-        const todoEditDescription = document.querySelector(`#todoEditDescription${todoIndex}`);
-        todo.description = todoEditDescription.value;
-        const todoEditPriority = document.querySelector(`#todoEditPriority${todoIndex}`);
-        todo.priority = todoEditPriority.value;
-        const todoEditDate = document.querySelector(`#todoEditDate${todoIndex}`);
-        todo.dueDate = todoEditDate.value;
+        submitTodoEdit(todoIndex, todo);
         clearMain();
         displayTodos(currentProject);
-
         event.target.textContent = "Edit";
     }
 }
@@ -259,6 +162,5 @@ function setupDefaults() {
     displayProject(projects[0], projects);
     displayTodos(projects[0]);
 }
-
 
 export { createNewProject, setupDefaults, createNewTodo, editTodo, selectProject, deleteTodo, deleteProject, toggleTodoDetails, editProject };
--- src/scripts/display.js
+++ src/scripts/display.js
@@ -214,4 +214,108 @@ function createNewTodoForm() {
     createTodoButtonDiv.appendChild(createTodoCancelButton);
 }
 
-export {displaySidebarContent, displayTodos, clearTodos, clearSidebar, clearNewProjectForm, clearMain, clearNewTodoForm, createNewProjectForm, displayProject, createNewTodoForm };
+function createTodoEditForm(todoDiv, todoIndex, todo) {
+    const todoEditDiv = document.createElement('div');
+    todoEditDiv.setAttribute('id', `todoEditDiv${todoIndex}`);
+    todoEditDiv.setAttribute('class', 'todoEditDiv');
+    todoDiv.appendChild(todoEditDiv);
+    const todoEditTitleLabel = document.createElement('label');
+    todoEditTitleLabel.setAttribute('for', `todoEditTitle${todoIndex}`);
+    todoEditTitleLabel.textContent = "Todo Title: "
+    todoEditDiv.appendChild(todoEditTitleLabel);
+    const todoEditTitle = document.createElement('input');
+    todoEditTitle.setAttribute('id', `todoEditTitle${todoIndex}`);
+    todoEditTitle.value = todo.title;
+    todoEditDiv.appendChild(todoEditTitle);
+    const todoEditDescriptionLabel = document.createElement('label');
+    todoEditDescriptionLabel.setAttribute('for', `todoEditDescription${todoIndex}`);
+    todoEditDescriptionLabel.textContent = "Todo Description: ";
+    todoEditDiv.appendChild(todoEditDescriptionLabel);
+    const todoEditDescription = document.createElement('textarea');
+    todoEditDescription.setAttribute('id', `todoEditDescription${todoIndex}`);
+    todoEditDescription.value = todo.description;
+    todoEditDiv.appendChild(todoEditDescription);
+    const todoEditPriorityLabel = document.createElement('label');
+    todoEditPriorityLabel.setAttribute('for', `todoEditPriority${todoIndex}`);
+    todoEditPriorityLabel.textContent = "Todo Priority: ";
+    todoEditDiv.appendChild(todoEditPriorityLabel);
+    const todoEditPriority = document.createElement('select');
+    todoEditPriority.setAttribute('id', `todoEditPriority${todoIndex}`);
+    todoEditDiv.appendChild(todoEditPriority);
+    const todoEditPriorityHigh = document.createElement('option');
+    todoEditPriorityHigh.setAttribute('value', 'High');
+    todoEditPriorityHigh.textContent = "High";
+    todoEditPriority.appendChild(todoEditPriorityHigh);
+    const todoEditPriorityMedium = document.createElement('option');
+    todoEditPriorityMedium.setAttribute('value', 'Medium');
+    todoEditPriorityMedium.textContent = "Medium";
+    todoEditPriority.appendChild(todoEditPriorityMedium);
+    const todoEditPriorityLow = document.createElement('option');
+    todoEditPriorityLow.setAttribute('value', 'Low');
+    todoEditPriorityLow.textContent = "Low";
+    todoEditPriority.appendChild(todoEditPriorityLow);
+    todoEditPriority.value = todo.priority;
+    const todoEditDateLabel = document.createElement('label');
+    todoEditDateLabel.setAttribute('for', `todoEditDate${todoIndex}`);
+    todoEditDateLabel.textContent = "Due Date: ";
+    todoEditDiv.appendChild(todoEditDateLabel);
+    const todoEditDate = document.createElement('input');
+    todoEditDate.setAttribute('id', `todoEditDate${todoIndex}`);
+    todoEditDate.setAttribute('type', 'date');
+    todoEditDate.value = todo.dueDate;
+    todoEditDiv.appendChild(todoEditDate);
+}
+
+function showTodoDetails(event, projects) {
+    const projectDiv = document.querySelector('.projectDiv');
+    const projectIndex = projectDiv.getAttribute('data-index');
+    const currentProject = projects[projectIndex];
+    const todoDiv = event.target.closest('.todoDiv');
+    const todoIndex = todoDiv.getAttribute('data-index');
+    const todo = currentProject.todos[todoIndex];
+    const todoDetailsDiv = document.createElement('div');
+    todoDetailsDiv.setAttribute('class', 'todoDetailsDiv');
+    todoDiv.appendChild(todoDetailsDiv);
+    let todoInfoDiv = document.createElement('div');
+    todoInfoDiv.setAttribute('class', 'todoInfoDiv');
+    todoDetailsDiv.appendChild(todoInfoDiv);
+    let todoDueDate = document.createElement('time');
+    todoDueDate.setAttribute('class', 'todoDueDate');
+    todoDueDate.setAttribute('datetime', todo.dueDate);
+    todoDueDate.textContent = `Due Date: ${todo.dueDate}`;
+    todoInfoDiv.appendChild(todoDueDate);
+    let todoPriority = document.createElement('p');
+    todoPriority.setAttribute('class', 'todoPriority');
+    todoPriority.textContent = `Priority: ${todo.priority}`;
+    todoInfoDiv.appendChild(todoPriority);
+    let todoDescription = document.createElement('p');
+    todoDescription.setAttribute('class', 'todoDescription');
+    todoDescription.textContent = todo.description;
+    todoDetailsDiv.appendChild(todoDescription);
+    event.target.textContent = "Hide Details";
+}
+
+function createEditProjectForm(projectHeader, projectDiv, currentProject) {
+    const projectTitleLabel = document.createElement('label');
+    projectTitleLabel.setAttribute('id', 'projectTitleLabel');
+    projectTitleLabel.setAttribute('for', 'projectTitleEdit');
+    projectTitleLabel.textContent = "Project Title: ";
+    projectHeader.prepend(projectTitleLabel);
+    const projectTitle = document.querySelector('.projectTitle');
+    const projectTitleInput = document.createElement('input');
+    projectTitleInput.setAttribute('id', 'projectTitleEdit');
+    projectTitleInput.value = currentProject.title;
+    projectHeader.replaceChild(projectTitleInput, projectTitle);
+    const projectDescription = document.querySelector('.projectDescription');
+    const projectDescriptionLabel = document.createElement('label');
+    projectDescriptionLabel.setAttribute('id', 'projectDescriptionLabel');
+    projectDescriptionLabel.setAttribute('for', 'projectDescriptionEdit');
+    projectDescriptionLabel.textContent = "Project Description: ";
+    projectDiv.insertBefore(projectDescriptionLabel, projectDescription);
+    const projectDescriptionInput = document.createElement('textarea');
+    projectDescriptionInput.setAttribute('id', 'projectDescriptionEdit');
+    projectDescriptionInput.value = currentProject.description; 
+    projectDiv.replaceChild(projectDescriptionInput, projectDescription);
+}
+
+export {displaySidebarContent, displayTodos, clearTodos, clearSidebar, clearNewProjectForm, clearMain, clearNewTodoForm, createNewProjectForm, displayProject, createNewTodoForm, createTodoEditForm, showTodoDetails, createEditProjectForm };
